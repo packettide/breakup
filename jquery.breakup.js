9c10515cb4b19a7f4a4858023fe83ee90a480f24
Max Walker <max@packettide.com>
2013-10-21 13:24:02 -0400

    Multiple breakup inputs on a page.

--- jquery.breakup.js
+++ jquery.breakup.js
@@ -8,28 +8,30 @@
 		length = length / (options.lengths || 1);
 		var maxLen = (options.lengths || 1);
 		this.before('<div class="breakupInputs"></div>');
+		var newEl = this.prev();
 		var max = 0;
 		for (var i = 0; i <= length - 1; i++) {
-			$('.breakupInputs').append('<input type="text" class="splitUp" maxlength="'+maxLen+'" data-index="'+i+'"/>');
+			newEl.append('<input type="text" class="breakup" maxlength="'+maxLen+'" data-index="'+i+'"/>');
 			max = i;
 		}
 		if (length !== Math.floor(length)) {
-			$('.breakupInputs').append('<input type="text" class="splitUp" maxlength="'+(parseInt(this.attr('maxlength'), 10) % (options.lengths || 1))+'" data-index="'+(i)+'"/>');
+			newEl.append('<input type="text" class="breakup" maxlength="'+(parseInt(this.attr('maxlength'), 10) % (options.lengths || 1))+'" data-index="'+(i)+'"/>');
 		}
-		$('input.splitUp').keyup(function () {
+		newEl.find('input.breakup').keyup(function () {
+			var parent = $(this).parent();
 			if((event.keyCode < 33  ||  event.keyCode > 90) && event.keyCode !== 8 ){
 				return ;
 			} else {
 				var nextIndex;
 				if ($(this).val().length >= $(this).attr('maxlength')) {
 					nextIndex = (parseInt($(this).attr('data-index'), 10) + 1);
-					$('input[data-index='+nextIndex+']').focus();
+					parent.find('input[data-index='+nextIndex+']').focus();
 				} else if ($(this).val().length === 0) {
 					nextIndex = (parseInt($(this).attr('data-index'), 10) - 1);
-					$('input[data-index='+nextIndex+']').focus();
+					parent.find('input[data-index='+nextIndex+']').focus();
 				}
 				that.val('');
-				$('input.splitUp').each(function () {
+				parent.find('input.breakup').each(function () {
 					that.val(that.val() + $(this).val());
 				});
 			}
